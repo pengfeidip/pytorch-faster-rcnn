corrected how Cascade RCNN inference, it average class scores of all rcnn_heads

lib/cascade_rcnn.py
@@ -75,9 +75,15 @@ class CascadeRCNN(nn.Module):
         '''
 
         rcnn_test_cfg=self.test_cfg.rcnn
+        cls_scores = []
         for i in range(self.num_stages):
             cur_rcnn_head=self.rcnn_head[i]
-            props, label, score = cur_rcnn_head.forward_test(feat, props, img_size)
+            props, label, cls_score = cur_rcnn_head.forward_test(feat, props, img_size)
+            cls_scores.append(cls_score)
+        cls_score = sum(cls_scores) / self.num_stages
+
+        soft = torch.softmax(cls_score, dim=1)
+        score, label = torch.max(soft, dim=1)
 
         num_classes = self.rcnn_head[0].num_classes
         nms_iou = self.test_cfg.rcnn.nms_iou

lib/heads.py
@@ -324,7 +324,7 @@ class BBoxHead(nn.Module):
         return refined
 
     def forward_test(self, feat, props, img_size=None):
-        logging.debug('START of BBoxHead refine_props'.center(50, '='))
+        logging.debug('START of BBoxHead forward_test'.center(50, '='))
         logging.debug('received props: {}'.format(props.shape))
         with torch.no_grad():
             cls_out, reg_out = self(feat, props)
@@ -346,6 +346,6 @@ class BBoxHead(nn.Module):
                 h, w = img_size
                 refined = torch.stack([refined[0].clamp(0, w), refined[1].clamp(0, h),
                                        refined[2].clamp(0, w), refined[3].clamp(0, h)])
-        logging.debug('END of BBoxHead refine_props'.center(50, '='))
-        return refined, label, score
+        logging.debug('END of BBoxHead forward_test'.center(50, '='))
+        return refined, label, cls_out
 