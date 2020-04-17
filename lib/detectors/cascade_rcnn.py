fix a bug where clamp_ does not do clamp inplace as tensor is still attached to some graph. also put target normalization and bbox clampping in param2bbox

lib/detectors/cascade_rcnn.py
@@ -129,7 +129,7 @@ class CascadeRCNN(nn.Module):
             
             if i < self.num_stages - 1:
                 with torch.no_grad():
-                    refined_props = cur_rcnn_head.refine_bboxes(tar_props, tar_labels, reg_outs, tar_is_gts)
+                    refined_props = cur_rcnn_head.refine_bboxes(tar_props, tar_labels, reg_outs, tar_is_gts, img_metas)
                     logging.debug('{}: refinded props: {}'.format(
                         class_name(self), ', '.join([str(rps.shape) for rps in refined_props])))
                     props = refined_props

lib/heads/bbox_head.py
@@ -114,14 +114,8 @@ class BBoxHead_v2(nn.Module):
         is_gt = is_gt.bool()
         props = props[:, ~is_gt]
         reg_out = reg_out.t()[:, ~is_gt]
-        param_mean = reg_out.new(self.target_means).view(4, -1)
-        param_std  = reg_out.new(self.target_stds).view(4, -1)
-        reg_out = reg_out * param_std + param_mean
-        refined = utils.param2bbox(props, reg_out)
-        if img_meta is not None:
-            H, W = img_meta['img_shape'][:2]
-            refined[[0, 2], :].clamp_(0.0, W-1)
-            refined[[1, 3], :].clamp_(0.0, H-1)
+        refined = utils.param2bbox(props, reg_out, self.target_means, self.target_stds,
+                                   img_meta['img_shape'] if img_meta is not None else None)
         return refined
 
     def predict_bboxes_single_image(self, props, cls_out, reg_out, img_size=None, cfg=None):
@@ -145,14 +139,7 @@ class BBoxHead_v2(nn.Module):
             else:
                 reg_out = reg_out.view(-1, 4, self.num_classes)
                 reg_out = reg_out[torch.arange(n_props), :, label]
-            param_mean = reg_out.new(self.target_means).view(-1, 4)
-            param_std  = reg_out.new(self.target_stds).view(-1, 4)
-            reg_out = reg_out * param_std + param_mean
-            preds = utils.param2bbox(props, reg_out.t())
-            if img_size is not None:
-                H, W = img_size
-                preds[[0, 2], :].clamp_(0.0, W-1)
-                preds[[1, 3], :].clamp_(0.0, H-1)
+            preds = utils.param2bbox(props, reg_out.t(), self.target_means, self.target_stds, img_size)
             if cfg is not None:
                 preds, score, label = utils.multiclass_nms_mmdet(
                     preds.t(), score, range(1, self.num_classes), cfg.nms_iou, cfg.min_score, cfg.max_per_img)

lib/heads/rpn_head.py
@@ -90,16 +90,7 @@ class RPNHead(AnchorHead):
                 cls_score = cls_score[topk_inds]
                 reg_out = reg_out[:, topk_inds]
                 anchor  = anchor[:, topk_inds]
-            param_means = reg_out.new(self.target_means).view(4, -1)
-            param_stds = reg_out.new(self.target_stds).view(4, -1)
-            reg_out = reg_out * param_stds + param_means
-            pred_bbox = utils.param2bbox(anchor, reg_out)
-            pred_bbox = torch.stack([
-                torch.clamp(pred_bbox[0], 0.0, W),
-                torch.clamp(pred_bbox[1], 0.0, H),
-                torch.clamp(pred_bbox[2], 0.0, W),
-                torch.clamp(pred_bbox[3], 0.0, H)
-            ])
+            pred_bbox = utils.param2bbox(anchor, reg_out, self.target_means, self.target_stds, img_size)
             if min_size > 0:
                 non_small = (pred_bbox[2]-pred_bbox[0] + 1 >= min_size) \
                             & (pred_bbox[3]-pred_bbox[1] + 1 >= min_size)

lib/region.py
@@ -225,13 +225,7 @@ class ProposalCreator(object):
             cls_out = rpn_cls_out.view(2, -1)
             reg_out = rpn_reg_out.view(4, -1)
             scores = torch.softmax(cls_out, 0)[1]
-            props_bbox = utils.param2bbox(anchors, reg_out)
-            props_bbox = torch.stack([
-                torch.clamp(props_bbox[0], 0.0, W),
-                torch.clamp(props_bbox[1], 0.0, H),
-                torch.clamp(props_bbox[2], 0.0, W),
-                torch.clamp(props_bbox[3], 0.0, H)
-            ])
+            props_bbox = utils.param2bbox(anchors, reg_out, img_size=img_size)
             small_area_mask = (props_bbox[2] - props_bbox[0] < min_size) \
                               | (props_bbox[3] - props_bbox[1] < min_size)
             num_small_area = small_area_mask.sum()

lib/utils.py
@@ -63,17 +63,42 @@ def bbox2param(base, bbox):
     tw, th = torch.log(bbox_w/base_w), torch.log(bbox_h/base_h)
     return torch.stack([tx, ty, tw, th])
 
-def param2bbox(base, param):
-    """
-    It calculates the bbox coordinates according to base bbox and parameters
+"""
+It applies delta to base bboxes.
 
-    Args:
-        base (tensor): the base bbox, of size (n, 4) where n is number of base bboxes
-              and 4 is (x_min, y_min, x_max, y_max) or (left, top, right, bottom)
-        param (tensor): parameters/distances (tx, ty, tw, th)
-    Returns:
-        the original bbox
-    """
+Args:
+    base [4, n]: the base bbox
+    param [4, n]: delta 
+    means and stds: this means params are normalized
+    img_size: if this is present, need to clamp size of bboxes
+Returns:
+    the original bbox
+"""
+def param2bbox(base, param, means=[0.0, 0.0, 0.0, 0.0], stds=[1.0, 1.0, 1.0, 1.0], img_size=None):
+    assert base.shape == param.shape
+    assert base.shape[0] == 4
+    means= base.new(means).view(4, -1)
+    stds = base.new(stds).view(4, -1)
+    param = param * stds + means
+    bbox = _param2bbox_(base, param)
+    if img_size is not None:
+        bbox = clamp_bbox(bbox, img_size)
+    return bbox
+
+def clamp_bbox(bbox, img_size):
+    '''
+    Args: 
+        bbox: [4, n], bboxes in xyxy order
+        img_size: [h, w], image size
+    '''
+    H, W = img_size[:2]
+    return torch.stack([
+        bbox[0].clamp(0.0, W-1),
+        bbox[1].clamp(0.0, H-1),
+        bbox[2].clamp(0.0, W-1),
+        bbox[3].clamp(0.0, H-1)])
+
+def _param2bbox_(base, param):
     assert base.shape == param.shape
     base_w, base_h = wh_from_xyxy(base)
     base_center_x, base_center_y = center_of(base)