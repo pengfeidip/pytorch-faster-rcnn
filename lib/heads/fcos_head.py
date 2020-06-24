fix a bug in giou calculation. GIoU tested to be OK with mAP 0.417

lib/heads/fcos_head.py
@@ -55,12 +55,11 @@ def simple_ltrb2bbox(ltrb, ctr_xy):
     x, y = ctr_xy
     return torch.stack([
         x - ltrb[0],
-        x + ltrb[2],
         y - ltrb[1],
+        x + ltrb[2],
         y + ltrb[3]
     ])
                     
-
 class FCOSHead(nn.Module):
     def __init__(self,
                  num_classes=21,
@@ -252,7 +251,6 @@ class FCOSHead(nn.Module):
         if self.use_giou:
             pos_reg_out = simple_ltrb2bbox(pos_reg_out, (0.0, 0.0))
             pos_reg_tar = simple_ltrb2bbox(pos_reg_tar, (0.0, 0.0))
-            
         reg_loss = self.loss_bbox(pos_reg_out, pos_reg_tar) / pos_reg 
 
         # next calc ctr loss

lib/losses.py
@@ -7,14 +7,11 @@ import numpy as np
 
 # pred [4, n], gt [4, n]
 def giou_loss(a, b):
-    print('in giou_loss'.center(50, '='))
-    print('a.shape:', a.shape, 'b.shape:', b.shape)
     assert a.shape[0] == 4 and b.shape[0] == 4 and a.shape == b.shape
     tl = torch.max(a[:2], b[:2])
     br = torch.min(a[2:], b[2:])
     area_i = torch.prod(br-tl, dim=0)
     area_i = area_i * (tl<br).all(0).float()
-    print('area_i:', area_i.tolist())
     area_a = torch.prod(a[2:]-a[:2], dim=0)
     area_b = torch.prod(b[2:]-b[:2], dim=0)
     area_u = area_a + area_b - area_i
@@ -25,8 +22,8 @@ def giou_loss(a, b):
         torch.max(a[2], b[2]),
         torch.max(a[3], b[3])])
     area_convex = torch.prod(convex[2:] - convex[:2], dim=0)
-    print('area_convex:', area_convex.tolist())
-    return iou - (area_convex - area_u) / area_convex
+    giou = iou - (area_convex - area_u) / area_convex
+    return 1 - giou
 
 
 def sigmoid_focal_loss(pred, target, alpha=0.25, gamma=2.0, fix_alpha=False):